5933677458fc23d6457ae6d04ff046c5f9d8d9de
Valter Negreiros <valternegreiros@gmail.com>
2026-03-25 18:50:32 -0300

    working

--- app/src/main/java/com/example/weather/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weather/repository/WeatherRepositoryImpl.kt
@@ -2,9 +2,9 @@ package com.example.weather.repository
 
 import com.example.weather.model.WeatherFirstResponse
 
-class WeatherRepositoryImpl : WeatherRepository() {
-
-    override suspend fun searchCity(query: String): WeatherFirstResponse {
-
-    }
-}
+//class WeatherRepositoryImpl : WeatherRepository() {
+//
+//    override suspend fun searchCity(query: String): WeatherFirstResponse {
+//
+//    }
+//}
