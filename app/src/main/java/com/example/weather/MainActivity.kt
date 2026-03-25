5d5cd986d80344d197f0d53b2cb8826127b839e2
Valter Negreiros <valternegreiros@gmail.com>
2026-03-25 18:15:52 -0300

    first commit

---
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.weather
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.weather.ui.theme.WeatherTheme
+
+class MainActivity : ComponentActivity() {
+
+    var city: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    TextField(
+                        value = city, // The current text to display
+                        onValueChange = { newText ->
+                            city = newText
+                        },
+                        label = { Text("Enter city here") } // Optional label/hint
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/weather/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.weather.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+
+class WeatherViewModel(viewModelScope: CoroutineScope) : ViewModel(viewModelScope) {
+
+
+}
